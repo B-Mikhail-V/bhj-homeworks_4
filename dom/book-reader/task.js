01589918eec02e9dbb72b06f01d6523d8b77de21
B-Mikhail-V <bakulin.mv@gmail.com>
2022-10-05 23:31:13 +0300

    Task_3-3

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,35 +1,44 @@
 const selectors = Array.from(document.querySelectorAll('.font-size'))
-// selectors[0].classList.add('font-size_active')
-// selectors[1].classList.remove('font-size_active')
-// console.log(selectors[0].classList);
-
-// находим индекс активного селектора
-const indexActiveSelector = function getActiveSelector() {
-    return selectors.findIndex(tab => tab.classList.contains('font-size_active'));
-  }
-
-
-//деактивация селектора по индексу
-function selectorDeact(array, idx) {
-    array[idx].classList.remove('font-size_active');
-}
-
-
-// selectorDeact(selectors, indexActiveSelector())
+const content = Array.from(document.getElementsByTagName('p'))
+console.log(content);
 
 selectors.forEach(element => {
-    console.log(element);
-    element.classList.toggle('font-size_active')
-    // selectorDeact(selectors, indexActiveSelector())
+    element.addEventListener('click', function (event) {
+    event.preventDefault()
+    selectorDeact(getActiveSelector())
+    if(!event.target.classList.contains('font-size_active')) {
+        event.target.classList.add('font-size_active')
+    }
+
+
+
+
+}) 
+});
     
 
-})
 
-// for(i = 0; i < selectors.length; i++) {
-//     selectors[i].addEventListener('click', function() {
-//         selectorDeact(indexActiveSelector());
-//         // selectors[i].classList.add('font-size_active')
-//         console.log(selectors[i].dataset.size);
-       
-//     })
-// }
+// находим индекс активного селектора
+function getActiveSelector() {
+    return selectors.findIndex(tab => tab.classList.contains('font-size_active'));
+  }
+// console.log(getActiveSelector());
+
+//деактивация селектора по индексу
+function selectorDeact(idx) {
+    selectors[idx].classList.remove('font-size_active');
+}
+
+//активация селектора
+function selectorAct(idx) {
+    selectors[idx].classList.add('font-size_active');
+}
+
+//изменение аттрибута контента
+function setText(param) {
+    content.forEach(element => {
+        element.classList.add(param)
+        console.log(element);
+    });
+}
+setText('font-size_small')
