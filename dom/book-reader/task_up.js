721e6049471496ac242bd39cf981a297637bac82
B-Mikhail-V <bakulin.mv@gmail.com>
2022-10-06 23:44:01 +0300

    Task_3-2 up

--- dom/book-reader/task_up.js
+++ dom/book-reader/task_up.js
@@ -1,21 +1,29 @@
 const selectors = Array.from(document.querySelectorAll('.font-size'))
 const content = Array.from(document.getElementsByTagName('p'))
+const content2 = document.getElementsByTagName('p')
 // const colorText = document.querySelector('.book__control_color').children
-// console.log(colorText);
+content.forEach(el => {
+    if(/^book_color/.test(el.classList)) {
+     console.log(el);   
+    }
+});
+    
+
+
 
 const el1 = Array.from(document.getElementsByTagName('a'))
 // console.log(el1);
 
 el1.forEach(element => {
     if(element.closest('.book__control_color')) {
-        console.log(element.dataset.textColor);
+        // console.log(element.dataset.textColor);
         
     }   
 });
 
 el1.forEach(element => {
     if(element.closest('.book__control_background')) {
-        console.log(element.dataset.bgColor);
+        // console.log(element.dataset.bgColor);
     }   
 });
 
@@ -23,18 +31,20 @@ el1.forEach(element => {
 el1.forEach(element => {
     element.addEventListener('click', function (event) {
     event.preventDefault()
-    // selectorDeact(getActiveSelector())
+    // resetText()
+    selectorDeact(getActiveSelector())
     if(!event.target.classList.contains('font-size_active')) {
-        selectorDeact(getActiveSelector())
+        // selectorDeact(getActiveSelector())
         event.target.classList.add('font-size_active')
         const fontSize = event.target.dataset.size
         setText(fontSize)
     }
-    if(!event.target.classList.contains('color_active') & event.target.dataset.textColor) {
+
+    if(!event.target.classList.contains('color_active') && event.target.dataset.textColor) {
         selectorTextColorDeact(getActiveTextColor())
         event.target.classList.add('color_active')
-        const fontColor = event.target.dataset.textColor
-        // setText(fontSize)
+        const textColor = event.target.dataset.textColor
+        setTextColor(textColor)
     }
 
 
@@ -44,35 +54,59 @@ el1.forEach(element => {
 
 // находим индекс активного селектора
 function getActiveSelector() {
-    return selectors.findIndex(tab => tab.classList.contains('font-size_active'));
+    return el1.findIndex(tab => tab.classList.contains('font-size_active'));
   }
 
   // находим индекс активного селектора цвета текста
   function getActiveTextColor() {
-    return selectors.findIndex(tab => tab.classList.contains('color_active'));
+    return el1.findIndex(tab => tab.classList.contains('color_active'));
   }
 
 //деактивация селектора по индексу
 function selectorDeact(idx) {
-    selectors[idx].classList.remove('font-size_active');
+    el1[idx].classList.remove('font-size_active');
 }
 
 // деактивация селектора цвета текста по индексу
 function selectorTextColorDeact(idx) {
-    selectors[idx].classList.remove('font-size_active');
+    el1[idx].classList.remove('color_active');
 }
 
+// сброс класса контента
+// function resetText() {
+//     content.forEach(element => {
+//         element.className = '';
+//     });
+// }
 
-//изменение аттрибута контента
+//изменение размера текста
 function setText(param) {
     content.forEach(element => {
-        if(param) {
-        element.classList = ('book font-size_'+ param)
-        // console.log(element);    
-        }
-         else {
-        element.className = 'book'   
-        }
+        element.classList.forEach(cl => {
+            if(/^font-size/.test(cl)) {
+                element.classList.remove(cl)   
+            }
+        });
+    if(param) {
+    element.classList.add('font-size_'+ param);    
+    } else {
+    element.classList.remove();
+    }
+    
+    });
+}
 
+//изменение цвета текста
+function setTextColor(param) {
+    content.forEach(element => {
+        element.classList.forEach(cl => {
+            if(/^book_color/.test(cl)) {
+                element.classList.remove(cl)   
+            }
+        });
+    if(param) {
+    element.classList.add('book_color-'+ param);    
+    }
+    
     });
 }
